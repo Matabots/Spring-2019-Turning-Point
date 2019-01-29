7c82bd1168074ee6ff62143aaf12ce8a2a4be9ef
Pablo Lepe <plepe00@gmail.com>
2019-01-28 18:59:35 -0800

    created Chewy

---
+++ Han/include/robot.hpp
@@ -0,0 +1,8 @@
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+
+extern Motor Catapult_1;
+extern Motor Catapult_2;
+extern Motor Intake;
+
+#endif
---
+++ Han/src/robot.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+#include "robot.hpp"
+
+Motor Catapult_1(, false);
+Motor Catapult_2(, true);
+Motor Intake(, false);
